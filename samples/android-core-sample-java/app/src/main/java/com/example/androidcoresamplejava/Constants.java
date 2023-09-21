bb4aa69f7d0d7d83520e1d5fad87107dfcc5e8d5
rohitkhirid <rohitkhirid@dyte.io>
2023-09-21 13:57:04 +0530

    feat: added java sample app

---
+++ samples/android-core-sample-java/app/src/main/java/com/example/androidcoresamplejava/Constants.java
@@ -0,0 +1,6 @@
+package com.example.androidcoresamplejava;
+
+public class Constants {
+  static String AUTH_TOKEN = "<ENTER-AUTH-TOKEN-HERE>";
+  static String BASE_URL = "https://api.dyte.io/v2";
+}
---
+++ samples/android-core-sample-java/app/src/main/java/com/example/androidcoresamplejava/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.androidcoresamplejava;
+
+import android.util.Log;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import io.dyte.core.DyteMeetingBuilder;
+import io.dyte.core.DyteMobileClient;
+import io.dyte.core.listeners.DyteMeetingRoomEventsListener;
+import io.dyte.core.models.DyteMeetingInfoV2;
+
+public class MainActivity extends AppCompatActivity {
+  private String TAG = "MainActivity";
+
+  private Button btnInit, btnJoin, btnLeave;
+  private ProgressBar pbLoader;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    btnInit = findViewById(R.id.btnInitMeeting);
+    btnJoin = findViewById(R.id.btnJoinMeeting);
+    btnLeave = findViewById(R.id.btnLeaveMeeting);
+    pbLoader = findViewById(R.id.pbLoader);
+
+    btnJoin.setVisibility(View.GONE);
+    btnLeave.setVisibility(View.GONE);
+
+    DyteMobileClient mobileClient = DyteMeetingBuilder.INSTANCE.build(this);
+    DyteMeetingRoomEventsListener meetingEventListener = new DyteMeetingRoomEventsListener() {
+      @Override public void onReconnectingToMeetingRoom() {
+
+      }
+
+      @Override public void onReconnectedToMeetingRoom() {
+
+      }
+
+      @Override public void onMeetingRoomReconnectionFailed() {
+
+      }
+
+      @Override public void onMeetingRoomDisconnected() {
+
+      }
+
+      @Override public void onMeetingRoomConnectionFailed() {
+
+      }
+
+      @Override public void onMeetingRoomLeaveStarted() {
+        Log.v(TAG, "onMeetingRoomLeaveStarted");
+      }
+
+      @Override public void onMeetingRoomLeaveCompleted() {
+        Log.v(TAG, "onMeetingRoomLeaveCompleted");
+      }
+
+      @Override public void onMeetingRoomJoinStarted() {
+        Log.v(TAG, "onMeetingRoomJoinStarted");
+        btnJoin.setEnabled(false);
+        pbLoader.setVisibility(View.VISIBLE);
+      }
+
+      @Override public void onMeetingRoomJoinFailed(@NonNull Exception e) {
+        Log.v(TAG, "onMeetingRoomJoinFailed");
+      }
+
+      @Override public void onMeetingRoomJoinCompleted() {
+        Log.v(TAG, "onMeetingRoomJoinCompleted");
+        pbLoader.setVisibility(View.GONE);
+        btnJoin.setVisibility(View.GONE);
+        btnLeave.setVisibility(View.VISIBLE);
+      }
+
+      @Override public void onMeetingInitStarted() {
+        Log.v(TAG, "onMeetingInitStarted");
+        btnInit.setEnabled(false);
+        pbLoader.setVisibility(View.VISIBLE);
+      }
+
+      @Override public void onMeetingInitFailed(@NonNull Exception e) {
+        Log.v(TAG, "onMeetingInitFailed " + e.getMessage());
+      }
+
+      @Override public void onMeetingInitCompleted() {
+        Log.v(TAG, "onMeetingInitCompleted");
+        pbLoader.setVisibility(View.GONE);
+        btnInit.setVisibility(View.GONE);
+        btnJoin.setVisibility(View.VISIBLE);
+      }
+
+      @Override public void onDisconnectedFromMeetingRoom() {
+
+      }
+
+      @Override public void onConnectingToMeetingRoom() {
+
+      }
+
+      @Override public void onConnectedToMeetingRoom() {
+
+      }
+    };
+
+    mobileClient.addMeetingRoomEventsListener(meetingEventListener);
+
+    btnInit.setOnClickListener(view -> {
+      Log.v(TAG, "doing init meeting");
+      DyteMeetingInfoV2 dyteMeetingInfoV2 = new DyteMeetingInfoV2(
+          Constants.AUTH_TOKEN,
+          true, true, Constants.BASE_URL);
+      mobileClient.init(dyteMeetingInfoV2);
+    });
+
+    btnJoin.setOnClickListener(view -> {
+      Log.v(TAG, "doing join meeting");
+      mobileClient.joinRoom();
+    });
+  }
+}
