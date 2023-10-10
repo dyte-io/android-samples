c1c17fd54a096c846015030fa1e7252bda0b861a
rohitkhirid <rohitkhirid@dyte.io>
2023-10-10 13:41:09 +0530

    feat: added dyte ui kit

---
+++ samples/android-ui-kit-sample-kotlin/app/src/main/java/io/dyte/androiduikitsamplekotlin/Constants.java
@@ -0,0 +1,6 @@
+package io.dyte.androiduikitsamplekotlin;
+
+public class Constants {
+  static String AUTH_TOKEN = "<YOUR-AUTH-TOKEN-HERE>";
+  static String BASE_URL = "https://api.dyte.io/v2";
+}
