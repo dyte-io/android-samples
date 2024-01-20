eb2c8a057b5bb0804c02e82c730379742f207a43
rohitkhirid <rohitkhirid@gmail.com>
2024-01-20 23:36:17 +0530

    chore: virtual bg sample

--- samples/android-ui-kit-sample-kotlin/app/src/main/java/io/dyte/androiduikitsamplekotlin/MainActivity.kt
+++ samples/android-ui-kit-sample-kotlin/app/src/main/java/io/dyte/androiduikitsamplekotlin/MainActivity.kt
@@ -1,11 +1,18 @@
 package io.dyte.androiduikitsamplekotlin
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.widget.Button
 import dyte.io.uikit.DyteUIKitBuilder
 import dyte.io.uikit.DyteUIKitInfo
+import io.dyte.core.listeners.DyteMeetingRoomEventsListener
 import io.dyte.core.models.DyteMeetingInfoV2
+import io.dyte.core.observability.DyteLogger
+import io.dyte.virtual_background.VirtualBackground
+import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
   private lateinit var btnStartMeeting: Button
@@ -30,6 +37,18 @@ class MainActivity : AppCompatActivity() {
       dyteMeetingInfo = meetingInfo
     )
     val dyteUIKit = DyteUIKitBuilder.build(dyteUIKitInfo)
+    dyteUIKit.meeting.addMeetingRoomEventsListener(object : DyteMeetingRoomEventsListener {
+      override fun onMeetingRoomJoinCompleted() {
+        super.onMeetingRoomJoinCompleted()
+        println("MainActivity::onMeetingRoomJoinCompleted::")
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.sample)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+        val imageBytes: ByteArray = byteArrayOutputStream.toByteArray()
+        val imageString: String = Base64.encodeToString(imageBytes, Base64.DEFAULT)
+        dyteUIKit.meeting.localUser.addVideoMiddleware(VirtualBackground(imageString))
+      }
+    })
     dyteUIKit.startMeeting()
   }
 }
